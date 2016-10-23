b30a1659b7ff65ad12491392d2e50b1c2e213ec7
MichaelMa <Minima2014@iCloud.com>
2016-10-23 20:06:56 +0800

    划分正负数据集
    不知道这样写行不行……

--- stategrid/data/__init__.py
+++ stategrid/data/__init__.py
@@ -19,7 +19,7 @@ def load_train_user_data():
     读入训练集数据
     :rtype: pandas.DataFrame
     """
-    return pandas.read_csv('train.csv')
+    return pandas.read_csv('data/train.csv', names=['CONS_NO', 'LABEL'])
 
 
 def load_test_user_data():
@@ -27,22 +27,47 @@ def load_test_user_data():
     读入测试集数据
     :rtype: pandas.DataFrame
     """
-    return pandas.read_csv('test_csv')
+    return pandas.read_csv('data/test_csv')
 
 
 def load_kwh_data():
     """
     :rtype: pandas.DataFrame
     """
-    if not os.path.exists('temp'):
-        os.mkdir('temp')
-    path = os.path.abspath('temp/user_kwh.csv')
-    if not os.path.exists(path):
-        df = pandas.read_csv('all_user_yongdian_data_2015.csv')
-        df['DATA_DATE'] = pandas.to_datetime(df['DATA_DATE'])
-        pt = pandas.pivot_table(df, values='KWH', index=['CONS_NO'], columns=['DATA_DATE'])
-        ptT = pt.T
-        ptT.fillna(ptT.median()) # fillna 按列补值
-        pt = ptT.T
-        pt.to_csv(path, index=True, quoting=csv.QUOTE_NONE)
-    return pandas.read_csv(path)
+    return pandas.read_csv('data/temp/user_kwh.csv')
+
+
+def load_pos_data():
+    """
+    :rtype: pandas.DataFrame
+    """
+    return pandas.read_csv('data/temp/pos.csv')
+
+
+def load_neg_data():
+    """
+    :rtype: pandas.DataFrame
+    """
+    return pandas.read_csv('data/temp/neg.csv')
+
+
+# 新建缓存
+if not os.path.exists('data/temp'):
+    os.mkdir('data/temp')
+
+df = pandas.read_csv('data/all_user_yongdian_data_2015.csv')
+df['DATA_DATE'] = pandas.to_datetime(df['DATA_DATE'])
+pt = pandas.pivot_table(df, values='KWH', index=['CONS_NO'], columns=['DATA_DATE'])
+ptT = pt.T
+ptT = ptT.fillna(ptT.median())  # fillna 按列补值
+pt = ptT.T
+pt.to_csv('data/temp/user_kwh.csv', index=True, quoting=csv.QUOTE_NONE)
+pt = pandas.read_csv('data/temp/user_kwh.csv')
+
+# 划分数据集
+train = load_train_user_data()
+m = pandas.merge(pt, train, on='CONS_NO')
+pos = m.loc[m.loc[:, 'LABEL'] == 1]
+neg = m.loc[m.loc[:, 'LABEL'] == 0]
+pos.to_csv('data/temp/pos.csv')
+neg.to_csv('data/temp/neg.csv')
